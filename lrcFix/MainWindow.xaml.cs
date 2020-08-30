cc1e798e3f6cdc4aaff949b13731a4c9c4b52553
sakuya <dummy@dummy>
2020-08-31 01:35:00 +0900

    Add main processing

---
+++ lrcFix/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lrcFix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DateTime Stopwatch;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PathTextBox.AddHandler(TextBox.DragOverEvent, new DragEventHandler(PathTextBox_DragOver), true);
+            PathTextBox.AddHandler(TextBox.DropEvent, new DragEventHandler(PathTextBox_Drop), true);
+        }
+
+        private void PathTextBox_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = false;
+        }
+
+        private void PathTextBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                PathTextBox.Text = string.Empty;
+                string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                PathTextBox.Text = filenames[0];
+            }
+        }
+
+        private void StopwatchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (StopwatchButton.Content.ToString() == "Stopwatch")
+            {
+                Stopwatch = DateTime.Now;
+                StopwatchButton.Content = "Stop";
+            }
+            else
+            {
+                TimeSpan time = (DateTime.Now - Stopwatch);
+                OffsetMinTextBox.Text = String.Format("{0:00}", time.Minutes);
+                OffsetSecTextBox.Text = String.Format("{0:00}", time.Seconds);
+                OffsetMilliSecTextBox.Text = String.Format("{0:00}", time.Milliseconds.ToString().PadRight(2, '0').Substring(0, 2));
+                StopwatchButton.Content = "Stopwatch";
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int min = int.Parse(OffsetMinTextBox.Text);
+            int sec = int.Parse(OffsetSecTextBox.Text);
+            int milliSec = int.Parse(OffsetMilliSecTextBox.Text + "0");
+            TimeSpan offsetTime = new TimeSpan(0, 0, min, sec, milliSec);
+            Processing.FixLrcFile(PathTextBox.Text, FasterRadio.IsChecked.Value,offsetTime);
+            MessageBox.Show("Success!", "OK");
+        }
+    }
+}
---
+++ lrcFix/Processing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Linq;
+
+namespace lrcFix
+{
+    class Processing
+    {
+        public static void FixLrcFile(string path,bool IsFaster,TimeSpan offsetTime)
+        {
+            //open lrc file
+            string[] lrcFile = File.ReadAllLines(path);
+            foreach (var (lineValue, index) in lrcFile.Select((value, index) => (value, index)))
+            {
+                //find  format [mm:ss.xx] (mm is minutes, ss is seconds and xx is hundredths of a second. )
+                MatchCollection match = Regex.Matches(lineValue, Regex.Escape("[") + "[0-9]{2}:[0-9]{2}.[0-9]{2}" + Regex.Escape("]"));
+                foreach (Match time in match)
+                {
+                    //set TimeSpan
+                    int m = int.Parse(time.Value.Substring(1, 2));
+                    int s = int.Parse(time.Value.Substring(4, 2));
+                    int ms = int.Parse(time.Value.Substring(7, 2) + "0");
+                    TimeSpan datatime = new TimeSpan(0, 0, m, s, ms);
+                    TimeSpan setTime;
+                    // calc time
+                    if (IsFaster)
+                    {
+                        setTime = datatime - offsetTime;
+                    }
+                    else
+                    {
+                        setTime = datatime + offsetTime;
+                    }
+                    //replace time
+                    string min = String.Format("{0:00}", Math.Max(setTime.Minutes, 0));
+                    string sec = String.Format("{0:00}", Math.Max(setTime.Seconds, 0));
+                    string millisec = String.Format("{0:00}", Math.Max(setTime.Milliseconds, 0).ToString().PadRight(2, '0').Substring(0, 2));
+                    lrcFile[index] = lrcFile[index].Replace(time.Value, $"[{min}:{sec}.{millisec}]");
+                }
+            }
+            File.WriteAllLines(path, lrcFile);
+        }
+    }
+}
